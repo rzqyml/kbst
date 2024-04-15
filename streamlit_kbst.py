687457b7b8f767831a5e7416f49963d8e5833797
Rizky Yaomal Malik <2010512081@mahasiswa.upnvj.ac.id>
2024-04-16 00:07:19 +0700

    Update streamlit_kbst.py

--- streamlit_kbst.py
+++ streamlit_kbst.py
@@ -91,6 +91,11 @@ if st.button('Lakukan Prediksi'):
         # Mengatur flag reset menjadi False setelah prediksi
         st.session_state.reset_flag = False
 
+# Tombol untuk menambahkan input data baru
+if st.button('Input Data Baru'):
+    # Menetapkan nilai default kembali
+    st.session_state.state = default_values.copy()
+
 # Menampilkan hasil prediksi
 st.success(f'Hasil Prediksi: {kbst_diagnosis}')
 
